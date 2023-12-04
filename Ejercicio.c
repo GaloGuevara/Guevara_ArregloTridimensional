ad1f00a242fac43484ec2d1d99dbfe0123923f27
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-12-04 16:26:31 -0500

    \Ejercicio.c Correcciones de diseño y documentación del codigo

--- Ejercicio.c
+++ Ejercicio.c
@@ -5,7 +5,7 @@ del arreglo en ceros, a excepción de la última matriz que debe ser inicializad
 
 int main(int argc, char const *argv[])
 {
-    int n, m, l;
+    int n, m, l; // n es el numero de matrices, m el numero de filas de cada matri y l el numero de columnas de cada matriz
     printf("Ingrese las dimensiones del primer arreglo: ");
     scanf("%d%*c", &n);
     printf("Ingrese las dimensiones del segundo arreglo: ");
@@ -35,7 +35,7 @@ int main(int argc, char const *argv[])
         {
             for (int k = 0; k < l; k++)
             {
-                if (i == n - 1)
+                if (i == n - 1) // Aqui seleccionamos la ultima matriz
                 {
                     ArregloTridimensional[i][j][k] = 1;
                 }
