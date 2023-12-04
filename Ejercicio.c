04c648d9e17820ea3e47df688d4f09f7efab76d9
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-12-04 16:13:11 -0500

    \Ejercicio.c Creación del enunciado del problema

---
+++ Ejercicio.c
@@ -0,0 +1,2 @@
+/*Crear e imprimir todos los elementos de un arreglo tridimensional, cuyas dimensiones sean ingresadas por el usuario. El programa que debe inicializar todas las matrices 
+del arreglo en ceros, a excepción de la última matriz que debe ser inicializada en unos*/
