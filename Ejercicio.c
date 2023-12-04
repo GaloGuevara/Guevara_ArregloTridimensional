86fd813c9f98f52586c41cd8b4d5c96e1d3140fc
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2023-12-04 16:15:49 -0500

    \Ejercicio.c Bloque de codigo donde el usuario ingresa las dimensiones de los arreglos

--- Ejercicio.c
+++ Ejercicio.c
@@ -1,2 +1,17 @@
 /*Crear e imprimir todos los elementos de un arreglo tridimensional, cuyas dimensiones sean ingresadas por el usuario. El programa que debe inicializar todas las matrices 
 del arreglo en ceros, a excepción de la última matriz que debe ser inicializada en unos*/
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char const *argv[])
+{
+    int i,j,k;
+    printf("Ingrese las dimensiones del primer arreglo: ");
+    scanf("%d%*c", &i);
+    printf("Ingrese las dimensiones del segundo arreglo: ");
+    scanf("%d%*c", &j);
+    printf("Ingrese las dimensiones del tercer arreglo: ");
+    scanf("%d%*c", &k);
+
+    return 0;
+}
